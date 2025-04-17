f9f9ab8b99ba520878f16a81df48205946b404d6
kerloye <guilleminkerloye@gmail.com>
2025-04-17 23:39:19 +0200

    low sort fix

---
+++ lownumsort.c
@@ -0,0 +1,31 @@
+void	three(t_stack a)
+{
+	int	greater;
+
+	greater = ;
+	if((a)->index == greater)
+		;
+	else if ((a)->next->index == greater)
+		;
+	if ((a)->index > (*a)->next->index)
+		;
+}
+
+void five(t_stack a, t_stack *b)
+{
+	int	size;
+
+	size = nodecount(a);
+	while (size --)
+	{
+		if ((a)->index == 0 || (a)->index ==1)
+			;
+		else
+			;
+	}
+	three(a);
+	;
+	;
+	if ((a)->index > (a)->next->index)
+		;
+}
