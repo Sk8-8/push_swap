ef42567103f0639c8233cdbe2d8f84af7d6cd2f6
kerloye <guilleminkerloye@gmail.com>
2025-04-30 18:11:03 +0200

    low arg fix in progress

--- lownumsort.c
+++ lownumsort.c
@@ -1,31 +1,31 @@
-void	three(t_stack a)
+void	three(t_node *a)
 {
 	int	greater;
 
 	greater = ;
 	if((a)->index == greater)
-		;
+		rotatenode(, 1);
 	else if ((a)->next->index == greater)
-		;
+		reverserotatenode(, 1);
 	if ((a)->index > (*a)->next->index)
-		;
+		swapfirstnode();
 }
 
-void five(t_stack a, t_stack *b)
+void five(t_node *a, t_node *b)
 {
 	int	size;
 
 	size = nodecount(a);
 	while (size --)
 	{
-		if ((a)->index == 0 || (a)->index ==1)
-			;
+		if ((a)->index == 0 || (a)->index == 1)
+			pushnode(, 2);
 		else
-			;
+			rotatenode(, 1);
 	}
 	three(a);
-	;
-	;
+	pushnode(, , 1);
+	pushnode(, , 1);
 	if ((a)->index > (a)->next->index)
-		;
+		swapfirstnode();
 }
